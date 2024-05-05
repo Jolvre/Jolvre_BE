17a62ede3d0eb25771080863ba7287097f38a80e
SystemUi <nick102030@naver.com>
2024-05-06 02:35:05 +0900

    [feat] : Comment 작성, 게시글의 Comment 불러오기 구현

--- src/main/java/com/example/jolvre/post/api/CommentController.java
+++ src/main/java/com/example/jolvre/post/api/CommentController.java
@@ -2,6 +2,7 @@ package com.example.jolvre.post.api;
 
 import com.example.jolvre.auth.entity.PrincipalDetails;
 import com.example.jolvre.post.dto.commentRequest;
+import com.example.jolvre.post.dto.commentResponse;
 import com.example.jolvre.post.repository.CommentRepository;
 import com.example.jolvre.post.service.CommentService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,6 +10,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -23,17 +28,29 @@ import java.util.List;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/post")
+@RequestMapping("/api/v1/comment")
 public class CommentController {
 
     private final CommentService commentService;
 
-//    @Operation(summary = "댓글 작성")
-//    @PostMapping("/{postId}/comment")
-//    public ResponseEntity<?> uploadComment(@PathVariable("postId") Long postId, @RequestBody commentRequest request,
-//                                           @AuthenticationPrincipal PrincipalDetails principalDetails) {
-//        commentService.WriteComment(postId, request, principalDetails.getUser());
-//
-//        return ResponseEntity.ok().build();
-//    }
+    @Operation(summary = "댓글 작성")
+    @PostMapping("/{postId}/comment")
+    public ResponseEntity<?> uploadComment(@PathVariable("postId") Long postId, @RequestBody commentRequest request,
+                                           @AuthenticationPrincipal PrincipalDetails principalDetails) {
+        commentService.writeComment(postId, request, principalDetails.getUser());
+
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<Page<commentResponse>> getAllComment(
+            @PathVariable("postId") Long postId,
+            @RequestParam(value = "page", defaultValue = "1") int page,
+            @RequestParam(value = "size", defaultValue = "5") int size,
+            Pageable pageable) {
+        pageable = PageRequest.of(page - 1, size, Sort.by("createdDate").ascending());
+        Page<commentResponse> commentList = commentService.findAllComment(pageable, postId);
+
+        return ResponseEntity.status(HttpStatus.OK).body(commentList);
+    }
 }
