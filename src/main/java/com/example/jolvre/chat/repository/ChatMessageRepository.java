f85d90d37e345a3dead2a95fd34e36df374b222e
김민서 <ms9648@naver.com>
2024-05-08 12:52:12 +0900

    [feat] 이전 채팅 불러오기 DB 접근 수정

--- src/main/java/com/example/jolvre/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/example/jolvre/chat/repository/ChatMessageRepository.java
@@ -2,7 +2,14 @@ package com.example.jolvre.chat.repository;
 
 import com.example.jolvre.chat.entity.ChatMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage,Long>{
+
+    @Query(value = "select * from chat_message where room_id = :room_id limit :defaultLoad",
+    nativeQuery = true)
+    public List<ChatMessage> findByRoomId(String room_id, int defaultLoad);
 
 }
