933f15b1a4a244cc7b9e885e0915c6f7e1a57d58
SystemUi <nick102030@naver.com>
2024-05-08 16:24:53 +0900

    [refactoring] : 댓글 삭제 기능 구현

--- src/main/java/com/example/jolvre/post/api/CommentController.java
+++ src/main/java/com/example/jolvre/post/api/CommentController.java
@@ -3,12 +3,9 @@ package com.example.jolvre.post.api;
 import com.example.jolvre.auth.entity.PrincipalDetails;
 import com.example.jolvre.post.dto.commentRequest;
 import com.example.jolvre.post.dto.commentResponse;
-import com.example.jolvre.post.repository.CommentRepository;
 import com.example.jolvre.post.service.CommentService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.persistence.EntityNotFoundException;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -18,12 +15,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.net.http.HttpHeaders;
-import java.util.List;
 
 @Tag(name = "Comment", description = "커뮤니티 댓글 API")
 @RestController
@@ -61,10 +53,5 @@ public class CommentController {
     public void deleteComment(@PathVariable("commentId") Long commentId,
                               @AuthenticationPrincipal PrincipalDetails principalDetails) {
         commentService.deleteComment(commentId);
-    }
-
-    @GetMapping("/comment/{commentId}")
-    public commentResponse test(@PathVariable("commentId") Long commentId) {
-        return commentService.getCommentById(commentId);
     }
 }
