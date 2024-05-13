781af43c457922325caf26c67676b0f47d99dd41
김민서 <ms9648@naver.com>
2024-05-13 14:06:06 +0900

    feat: 이전 내역 불러오기

--- src/main/java/com/example/jolvre/chat/api/ChatController.java
+++ src/main/java/com/example/jolvre/chat/api/ChatController.java
@@ -1,6 +1,7 @@
 package com.example.jolvre.chat.api;
 
 import com.example.jolvre.auth.entity.PrincipalDetails;
+import com.example.jolvre.chat.dto.ChatRoomDto.ChatMessageRequest;
 import com.example.jolvre.chat.entity.ChatMessage;
 import com.example.jolvre.chat.entity.ChatRoom;
 import com.example.jolvre.chat.entity.ChatRoomMember;
@@ -8,12 +9,15 @@ import com.example.jolvre.chat.repository.ChatMessageRepository;
 import com.example.jolvre.chat.repository.ChatRoomMemberRepository;
 import com.example.jolvre.chat.repository.ChatRoomRepository;
 import com.example.jolvre.user.entity.User;
+import com.example.jolvre.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 
@@ -28,27 +32,26 @@ public class ChatController {
     private final ChatRoomRepository chatRoomRepository;
     private final ChatMessageRepository chatMessageRepository;
     private final ChatRoomMemberRepository chatRoomMemberRepository;
+    private final UserRepository userRepository;
 
     @MessageMapping("/chat/{roomId}")
     @SendTo("/sub/chat/{roomId}")
-    public void message(@AuthenticationPrincipal PrincipalDetails principalDetails, ChatMessage message, @DestinationVariable("roomId") String roomId){
-        log.info("chat {} send by {} to room number{}", message.getMessage(), message.getMessageId(), roomId);
-        System.out.println(chatRoomRepository.findByRoomId(roomId).getRoomId());
-        System.out.println(LocalDateTime.now());
+    public void message(ChatMessageRequest message, @DestinationVariable("roomId") String roomId){
+        User sender = userRepository.findByEmail(message.getSender()).get();
         ChatMessage chatMessage = ChatMessage.builder()
                         .message(message.getMessage())
-                        .sender(principalDetails.getUser())
                         .sendTime(LocalDateTime.now())
+                        .sender(sender)
                         .roomId(chatRoomRepository.findByRoomId(roomId))
                         .build();
-
+        System.out.println(chatMessage);
         chatMessageRepository.save(chatMessage);
 
         messagingTemplate.convertAndSend("/sub/chat/"+roomId, chatMessage);
         // 상대방에게 알림 보내기
-        ChatRoomMember chatRoomMembers = chatRoomMemberRepository.findByRoomIdAndNotSender(roomId, 17L);
-        User receiver = chatRoomMembers.getMember();
-        System.out.println(receiver.getId());
-        messagingTemplate.convertAndSend("/sub/chat/"+receiver.getId(), chatMessage);
+//        ChatRoomMember chatRoomMembers = chatRoomMemberRepository.findByRoomIdAndNotSender(roomId, 17L);
+//        User receiver = chatRoomMembers.getMember();
+//        System.out.println(receiver.getId());
+//        messagingTemplate.convertAndSend("/sub/chat/"+receiver.getId(), chatMessage);
     }
 }
