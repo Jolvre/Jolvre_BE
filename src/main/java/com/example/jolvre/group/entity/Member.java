e4a0ac3436bebbf755413126a54fe18478a6f925
gosu <q63530@naver.com>
2024-05-31 14:06:49 +0900

    [refactor] : Manager Deprecated
    기존
    - GroupExhibit 에서 Manager 컬럼을 통해 매니저를 관리했음
    
    변경
    - Manager 컬럼을 Member 컬럼에 통합하고 , Member에 GroupRole 컬럼을 통해 관리하도록 변경

--- src/main/java/com/example/jolvre/group/entity/Member.java
+++ src/main/java/com/example/jolvre/group/entity/Member.java
@@ -4,6 +4,8 @@ import com.example.jolvre.common.entity.BaseTimeEntity;
 import com.example.jolvre.user.entity.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -32,9 +34,14 @@ public class Member extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
+    @Column
+    @Enumerated(EnumType.STRING)
+    private GroupRole groupRole;
+
     @Builder
-    public Member(GroupExhibit groupExhibit, User user) {
+    public Member(GroupExhibit groupExhibit, User user, GroupRole groupRole) {
         this.groupExhibit = groupExhibit;
         this.user = user;
+        this.groupRole = groupRole;
     }
 }
