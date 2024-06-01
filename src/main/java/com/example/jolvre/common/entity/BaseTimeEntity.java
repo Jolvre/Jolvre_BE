78ecf501b6b9d748c2d9249a3c5726d72de6a40f
gosu <q63530@naver.com>
2024-06-01 10:33:41 +0900

    [fix] : 타임존 설정

--- src/main/java/com/example/jolvre/common/entity/BaseTimeEntity.java
+++ src/main/java/com/example/jolvre/common/entity/BaseTimeEntity.java
@@ -1,11 +1,13 @@
 package com.example.jolvre.common.entity;
 
+import jakarta.persistence.Convert;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters.LocalDateTimeConverter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
@@ -15,8 +17,10 @@ public abstract class BaseTimeEntity {
     // 등록일, 수정일을 공용으로 맵핑하기 위한 엔티티
 
     @CreatedDate
+    @Convert(converter = LocalDateTimeConverter.class)
     private LocalDateTime createdDate; // 등록일
 
     @LastModifiedDate
+    @Convert(converter = LocalDateTimeConverter.class)
     private LocalDateTime lastModifiedDate; // 수정일
 }
